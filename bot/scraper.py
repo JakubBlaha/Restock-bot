0b9e0834fdf9e4ea712b9688336b799e51ce0ea2
JakubBlaha <jakub.blaha@gmail.com>
2020-05-14 14:59:56 +0200

    Enabled price and url scraping

--- bot/scraper.py
+++ bot/scraper.py
@@ -53,15 +53,15 @@ class SupremeScraper(Scraper):
             if item.title + item.description + item.dt in ignored:
                 continue
 
-            # name_tag.click()
+            name_tag.click()
 
-            # self.d.switch_to.window(self.d.window_handles[-1])
+            self.d.switch_to.window(self.d.window_handles[-1])
 
-            # item.url = self.d.current_url
-            # item.price = WebDriverWait(self.d, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'price'))).get_attribute('innerText')
+            item.url = self.d.current_url
+            item.price = WebDriverWait(self.d, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'price'))).get_attribute('innerText')
 
-            # self.d.close()
-            # self.d.switch_to.window(self.d.window_handles[0])
+            self.d.close()
+            self.d.switch_to.window(self.d.window_handles[0])
 
             items.append(item)
 
